9d887193040fdc607e687cd93dccee44d0fda06d
remis <r.pankevicius@gmail.com>
2014-10-31 16:32:03 +0200

    Fixed AssemblyCopyright

--- src/mortals/contestants/team2-Samagon/Samagon.BigNumber/Properties/AssemblyInfo.cs
+++ src/mortals/contestants/team2-Samagon/Samagon.BigNumber/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Samagon.BigNumber")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ESRI")]
+[assembly: AssemblyCompany("sgrouphackathon")]
 [assembly: AssemblyProduct("Samagon.BigNumber")]
-[assembly: AssemblyCopyright("Copyright © ESRI 2014")]
+[assembly: AssemblyCopyright("Copyright sgrouphackathon 2014, see LICENSE")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
