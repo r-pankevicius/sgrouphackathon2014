8c4d668b7882d8f826706257dd6df7df2f0e2250
remis <r.pankevicius@gmail.com>
2014-11-03 18:32:26 +0200

    Fixed license

--- src/mortals/contestants/team2-Samagon/Samagon.BigNumber/Streaming/NumberFileInfo.cs
+++ src/mortals/contestants/team2-Samagon/Samagon.BigNumber/Streaming/NumberFileInfo.cs
@@ -1,14 +1,3 @@
-﻿/*****************************************************************************
-Created......: 10/20/2014 7:00:02 PM
-Author.......: dabu, S-GROUP Solutions
-
-COPYRIGHT (c) 2014 S-GROUP Solutions. ALL RIGHTS RESERVED.
-
-THE SOURCE CODE MAY BE USED AND COPIED ONLY WITH THE WRITTEN PERMISSION FROM
-S-GROUP Solutions OR IN ACCORDANCE WITH THE TERMS AND CONDITIONS STIPULATED
-IN THE AGREEMENT/CONTRACT UNDER WHICH THE SOURCE CODE HAVE BEEN SUPPLIED.
-*****************************************************************************/
-
 using System;
 using System.Collections.Generic;
 using System.Text;
